5e0fa0f1f520d1c825d98287436162451fdab4e6
DarrelA <darrelaiscoding@gmail.com>
2024-06-13 12:06:00 +0800

    `authServiceRouter`: Health check & 404 - Not Found

--- app/router.auth.go
+++ app/router.auth.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"log"
+
 	"github.com/DarrelA/starter-go-postgresql/internal/handlers"
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,5 +12,18 @@ func authServiceRouter() {
 		router.Post("/register", handlers.Register)
 		router.Post("/login", handlers.Login)
 		router.Get("/refresh", handlers.RefreshAccessToken)
+	})
+
+	authService.Get("/health", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success"})
+	})
+
+	authService.All("*", func(c *fiber.Ctx) error {
+		path := c.Path()
+		log.Printf("Invalid Path: %v", path)
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  "fail",
+			"message": "404 - Not Found",
+		})
 	})
 }
