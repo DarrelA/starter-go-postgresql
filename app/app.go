ef82a47608cdab36f6e41e9d0c24876c6f5094f5
DarrelA <darrelaiscoding@gmail.com>
2024-06-27 08:50:39 +0800

    Refactor instances initialization logic

--- app/app.go
+++ app/app.go
@@ -6,57 +6,66 @@ import (
 
 	"github.com/DarrelA/starter-go-postgresql/configs"
 	"github.com/DarrelA/starter-go-postgresql/db"
+	pgdb "github.com/DarrelA/starter-go-postgresql/db/pgdb"
+	redisDb "github.com/DarrelA/starter-go-postgresql/db/redis"
 	"github.com/DarrelA/starter-go-postgresql/internal/middlewares"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/rs/zerolog/log"
 )
 
-func StartApp(rdbms db.RDBMS, inMemoryDb db.InMemoryDB) *fiber.App {
-	log.Debug().Msg("starting StartApp function")
-	createDBConnections(rdbms, inMemoryDb)
-	app, authService := createFiberInstances()
-	useMiddleware(authService)
-	authServiceRouter(authService)
-	return app
+var (
+	rdbmsInstance       db.RDBMS
+	inMemoryDbInstance  db.InMemoryDB
+	appInstance         *fiber.App
+	authServiceInstance *fiber.App
+)
+
+func CreateDBConnections() (db.RDBMS, db.InMemoryDB) {
+	if rdbmsInstance == nil {
+		rdbmsInstance = pgdb.NewDB()
+	}
+
+	if inMemoryDbInstance == nil {
+		inMemoryDbInstance = redisDb.NewDB()
+	}
+
+	rdbmsInstance.Connect()
+	inMemoryDbInstance.Connect()
+
+	return rdbmsInstance, inMemoryDbInstance
 }
 
-func CloseConnections(app *fiber.App, rdbms db.RDBMS, inMemoryDb db.InMemoryDB) {
-	log.Debug().Msg("starting CloseConnections function")
-
+func CloseConnections() {
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
-	if err := app.ShutdownWithContext(ctx); err != nil {
+	if err := appInstance.ShutdownWithContext(ctx); err != nil {
 		log.Err(err).Msg("failed to gracefully shutdown the server")
 	}
+
 	cancel()
-	log.Debug().Msg("app instance has shutdown")
+	log.Info().Msg("app instance has shutdown")
 
-	pgdb := rdbms
-	redisDb := inMemoryDb
-
-	pgdb.Disconnect()
-	redisDb.Disconnect()
-	log.Debug().Msg("end of CloseConnections function")
+	rdbmsInstance.Disconnect()
+	inMemoryDbInstance.Disconnect()
 }
 
-func createDBConnections(rdbms db.RDBMS, inMemoryDb db.InMemoryDB) {
-	pgdb := rdbms
-	redisDb := inMemoryDb
-	pgdb.Connect()
-	redisDb.Connect()
-	log.Debug().Msg("connected to databases")
+// ConfigureAppInstance sets up and configures instances of Fiber for the main app and auth service,
+// including middleware and routing for authentication.
+func ConfigureAppInstance() (*fiber.App, *fiber.App) {
+	log.Info().Msg("creating fiber instances, connecting middleware & router")
+	appInstance = fiber.New()
+	authServiceInstance = fiber.New()
+	appInstance.Mount("/auth", authServiceInstance)
+
+	useMiddlewares(authServiceInstance)
+	authServiceRouter(authServiceInstance)
+
+	log.Debug().Msgf("appInstance memory address: %p", appInstance)
+	return appInstance, authServiceInstance
 }
 
-func createFiberInstances() (*fiber.App, *fiber.App) {
-	app := fiber.New()
-	authService := fiber.New()
-	log.Debug().Msg("created app and authService instance")
-	app.Mount("/auth", authService)
-	return app, authService
-}
-
-func useMiddleware(authService *fiber.App) {
-	authService.Use(cors.New(cors.Config{
+func useMiddlewares(authServiceInstance *fiber.App) {
+	authServiceInstance.Use(cors.New(cors.Config{
 		AllowOrigins:     configs.CORSSettings.AllowedOrigins,
 		AllowMethods:     "GET,POST",
 		AllowHeaders:     "Content-Type",
@@ -64,7 +73,8 @@ func useMiddleware(authService *fiber.App) {
 		AllowCredentials: true,
 		MaxAge:           12 * 60 * 60,
 	}))
-	authService.Use(middlewares.CorrelationAndRequestID)
-	authService.Use(middlewares.LogRequest)
-	log.Debug().Msg("applied middlewares to authService")
+
+	authServiceInstance.Use(middlewares.CorrelationAndRequestID)
+	authServiceInstance.Use(middlewares.LogRequest)
+	log.Info().Msg("applied middlewares to authServiceInstance")
 }
--- main.go
+++ main.go
@@ -3,21 +3,17 @@ package main
 import (
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/DarrelA/starter-go-postgresql/app"
 	"github.com/DarrelA/starter-go-postgresql/configs"
-	"github.com/DarrelA/starter-go-postgresql/db"
-	pgdb "github.com/DarrelA/starter-go-postgresql/db/pgdb"
-	redisDb "github.com/DarrelA/starter-go-postgresql/db/redis"
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	log.Debug().Msg("Starting main function")
-
 	// Create a logger instance with output to a file
 	logFile, err := os.Create("/app/logs/app.log")
 	if err != nil {
@@ -33,30 +29,39 @@ func main() {
 		Timestamp().
 		Logger()
 
-	// Configure Instances
-	rdbmsInstance := pgdb.NewDB()
-	inMemoryDbInstance := redisDb.NewDB()
-	appInstance := app.StartApp(rdbmsInstance, inMemoryDbInstance)
+	// Use `WaitGroup` when you just need to wait for tasks to complete without exchanging data.
+	// Use channels when you need to signal task completion and possibly exchange data.
+	var wg sync.WaitGroup
+	wg.Add(1)
 
+	var appInstance *fiber.App
+
+	go func() {
+		defer wg.Done()
+		app.CreateDBConnections()
+		appInstance, _ = app.ConfigureAppInstance()
+	}()
+
+	wg.Wait()
 	go startServer(appInstance)
-	waitForShutdown(appInstance, rdbmsInstance, inMemoryDbInstance)
+	waitForShutdown()
 
 	logFile.Close()
 	os.Exit(0)
 }
 
 func startServer(appInstance *fiber.App) {
-	log.Debug().Msg("listening at port: " + configs.Port)
+	log.Info().Msg("listening at port: " + configs.Port)
 	err := appInstance.Listen(":" + configs.Port)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to start server")
 	}
 }
 
-func waitForShutdown(appInstance *fiber.App, rdbmsInstance db.RDBMS, inMemoryDbInstance db.InMemoryDB) {
+func waitForShutdown() {
 	sigChan := make(chan os.Signal, 1) // Create a channel to listen for OS signals
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 	<-sigChan // Block until a signal is received
 	log.Debug().Msg("received termination signal, shutting down")
-	app.CloseConnections(appInstance, rdbmsInstance, inMemoryDbInstance)
+	app.CloseConnections()
 }
