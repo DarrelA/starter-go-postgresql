bed8702b57c4d8e46ba121e2fd55c6d371477f7e
DarrelA <darrelaiscoding@gmail.com>
2024-07-02 08:39:43 +0800

    COPY `sql` scripts for database seeding in `Dockerfile` and `Dockerfile.test`

---
+++ build/sql/seed.user.dev.sql
@@ -0,0 +1,21 @@
+INSERT INTO
+  users (first_name, last_name, email, password)
+VALUES
+  (
+    'Carlyn',
+    'Daniel',
+    'Carlyn_Daniel@gmail.com',
+    'Password1!'
+  ),
+  (
+    'Carlyn',
+    'Daniel',
+    'Carlyn_Daniël@gmail.com',
+    'Password1!'
+  ),
+  (
+    'Emily',
+    'Clark',
+    'Emily_Clark@gmail.com',
+    'Password1!'
+  );
